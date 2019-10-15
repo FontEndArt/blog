a66fff79b5dfdf5c280d392d6d742c6961746b16
武浩然 <1252198830@qq.com>
2019-10-15 22:33:21 +0800

    commit blog

--- scripts/push/blog.sh
+++ scripts/push/blog.sh
@@ -11,11 +11,12 @@ echo '# 前端进阶与被虐笔记'  >> README.md
 echo '作者：武浩然@前端小然子'  >> README.md
 echo '地址：[https://xiaoranzife.com](https://xiaoranzife.com 前端进阶与被虐笔记)'  >> README.md
 
+
 git add .
 git commit -m "$1"
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f https://github.com/FontEndArt/FontEndArt.github.io.git master
+git push blog master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
