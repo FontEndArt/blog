f598ab2b8e25db7d4a3aba08565f20057171a1c4
武浩然 <1252198830@qq.com>
2019-10-15 22:31:25 +0800

    commit blog

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 前端进阶与被虐笔记
 作者：武浩然@前端小然子
 地址：[https://xiaoranzife.com](https://xiaoranzife.com 前端进阶与被虐笔记)
+# 前端进阶与被虐笔记
+作者：武浩然@前端小然子
+地址：[https://xiaoranzife.com](https://xiaoranzife.com 前端进阶与被虐笔记)
--- scripts/push/blog.sh
+++ scripts/push/blog.sh
@@ -19,5 +19,3 @@ git push -f https://github.com/FontEndArt/FontEndArt.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-cd -
