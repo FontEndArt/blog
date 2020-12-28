b17dd8ac6b7d83652f98e3335a862179fd51052e
武浩然 <wuhaoran@kaikeba.com>
2020-12-28 19:40:26 +0800

    "COMMIT: wip: 并查集"

--- docs/.vuepress/router/91.js
+++ docs/.vuepress/router/91.js
@@ -1,35 +1,39 @@
 module.exports = [
+    // {
+    //     title: '讲义',
+    //     collapsable: false,
+    //     children:
+    //         [
+    //             'lecture/数组',
+    //             // '0.pointer',
+    //             // '0.structure',
+    //             // '0.复杂度',
+    //             // '0.递归',
+    //             // '0.分治',
+    //             // '1.TwoSum',
+    //             // '2.AddTwoNumbers',
+    //             // '3.lengthOfLongestSubstring',
+    //         ]
+    // },
     {
-        title: '讲义',
+        title: "小然子有话说",
         collapsable: false,
-        children:
-            [
-                'lecture/数组',
-                // '0.pointer',
-                // '0.structure',
-                // '0.复杂度',
-                // '0.递归',
-                // '0.分治',
-                // '1.TwoSum',
-                // '2.AddTwoNumbers',
-                // '3.lengthOfLongestSubstring',
-            ]
+        children: ["share/谈谈对并查集的理解.md"],
     },
     {
-        title: 'LeetCode',
+        title: "LeetCode",
         collapsable: false,
-        children:
-            [
-                'leetcode/75.颜色分类.md',
-                'leetcode/380.常数时间插入、删除和获取随机元素',
-                // '0.pointer',
-                // '0.structure',
-                // '0.复杂度',
-                // '0.递归',
-                // '0.分治',
-                // '1.TwoSum',
-                // '2.AddTwoNumbers',
-                // '3.lengthOfLongestSubstring',
-            ]
-    }
-]
+        children: [
+            "leetcode/75.颜色分类.md",
+            "leetcode/380.常数时间插入、删除和获取随机元素",
+            // '0.pointer',
+            // '0.structure',
+            // '0.复杂度',
+            // '0.递归',
+            // '0.分治',
+            // '1.TwoSum',
+            // '2.AddTwoNumbers',
+            // '3.lengthOfLongestSubstring',
+        ],
+    },
+];
